44f90529e346cb1447bf44bfcc57fc5dba647814
Maria Maas <meagmaas@student.che.nl>
2025-03-07 09:10:47 +0100

    try again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/Dracey.github.io/',
+    base: '/dracey.github.io/',
     resolve: {
 
     }
